cf6aba15aaefa8eb3464652eeda9e8bebdca3f5a
KazuDon-0407 <56216176+KazuDon-0407@users.noreply.github.com>
2020-03-17 13:45:23 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,1 @@
+javascript:void((function(){var d=document;d.body.innerHTML=d.body.innerHTML.replace(/Hayabusa/g,'Hayabuaaa');d.body.innerHTML=d.body.innerHTML.replace(/Yamajet/g,'Yaaajet');d.body.innerHTML=d.body.innerHTML.replace(/\/wordpress\/wp-content\/uploads\/2019\/08\/20190809_news_At36HsZi_05.png/g,'https:\/\/pbs.twimg.com\/media\/ETPskGxUwAI8Jet?format=jpg');})());
