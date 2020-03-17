c213d6ae776814c56ec6a15b7f63e37e485d4baf
KazuDon-0407 <56216176+KazuDon-0407@users.noreply.github.com>
2020-03-17 13:50:46 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 javascript:void((function(){
   var d=document;
   d.body.innerHTML=d.body.innerHTML.replace(/Hayabusa/g,'Hayabuaaa');
-  d.body.innerHTML=d.body.innerHTML.replace(/Yamajet/g,'Yaaajet');
+  d.body.innerHTML=d.body.innerHTML.replace(/Yamajet/g,'Yaaaajet');
   d.body.innerHTML=d.body.innerHTML.replace(/\/wordpress\/wp-content\/uploads\/2019\/08\/20190809_news_At36HsZi_05.png/g,'https:\/\/pbs.twimg.com\/media\/ETSP4E3UcAAVgY3?format=jpg');
 })());
